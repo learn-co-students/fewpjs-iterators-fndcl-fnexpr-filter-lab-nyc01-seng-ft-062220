10d09603b517928c1e53f771cbeb008be8c937df
axiumone <axiumone@gmail.com>
2020-08-13 20:36:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter(x => 
+         x.toLowerCase() === string.toLowerCase()
+    )
+}
+
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(x => 
+         x.slice(0, 1) === string.slice(0, 1)
+    )
+}
+
+function matchName(drivers, string){
+    return drivers.filter(x => 
+         x.name === string
+    )
+}
